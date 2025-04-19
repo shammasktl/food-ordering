409cbb538d74a995768d9e6630948df5f2033318
shammasktl <mshammast370737@gmail.com>
2025-04-20 00:43:43 +1200

    feat(Header): add logo and cart button with styling.

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from '../assets/logo.jpg'
+
+const Header = () => {
+  return (
+    <header id='main-header'>
+      <div id='title'>
+        <img src={logo} alt="" />
+        <h1>Ipsum</h1>
+      </div>
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
