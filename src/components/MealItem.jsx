bdf010fca48169b5b0407e6052f91b9d63246d65
shammasktl <mshammast370737@gmail.com>
2025-04-20 04:51:22 +1200

    feat(components): complete MealItem component with UI and functionality

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
-const MealItem = () => {
+const MealItem = ({meal}) => {
   return (
-    <div>
-      
-    </div>
+    <li className='meal-item'>
+      <article>
+        <img src={meal.image} alt="" />
+        <div>
+            <h3>{meal.name}</h3>
+            <p className='meal-item-price'>{meal.price}</p>
+            <p className='meal-item-description'>{meal.description}</p>
+        </div>
+        <p className='meal-item-actions'>
+            <button>Add to Cart</button>
+        </p>
+      </article>
+    </li>
   )
 }
 
