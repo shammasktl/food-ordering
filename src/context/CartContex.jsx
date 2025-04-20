cbf464077402b245b46c0e365dded0a2c8aba763
shammasktl <mshammast370737@gmail.com>
2025-04-20 19:14:51 +1200

    feat(Context): add CartContextProvider component with basic provider setup

--- src/context/CartContex.jsx
+++ src/context/CartContex.jsx
@@ -4,4 +4,12 @@ const CartContext = createContext({
     items : [],
     addItem : (item) => {},
     removeItem : (id) => {},
-})
+});
+
+const CartContextProvider = ({children}) => {
+    return (
+        <CartContext.Provider>{children}</CartContext.Provider>
+    )
+};
+
+export default CartContextProvider;
