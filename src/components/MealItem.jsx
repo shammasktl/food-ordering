aebf254073e27afb83c3cc363569c75eb0094b4a
shammasktl <mshammast370737@gmail.com>
2025-04-20 15:53:57 +1200

    feat: integrate reusable Button component in Header and MealItem with styled variants

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { formatCurrency } from '../util/currencyFormatter'
+import Button from './UI/Button'
 
 const MealItem = ({meal}) => {
   return (
@@ -12,7 +13,7 @@ const MealItem = ({meal}) => {
             <p className='meal-item-description'>{meal.description}</p>
         </div>
         <p className='meal-item-actions'>
-            <button>Add to Cart</button>
+          <Button>Add to Cart</Button>
         </p>
       </article>
     </li>
