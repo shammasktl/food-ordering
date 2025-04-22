663dbe149fd072663751e06dd934f6ae99949c5a
shammasktl <mshammast370737@gmail.com>
2025-04-23 00:31:00 +1200

    chore(CartItem): add new component for cart item in modal with increasing and decreasing quantity.

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { formatCurrency } from '../util/currencyFormatter'
+import { Trash2 } from 'lucide-react'
+import { useCart } from '../context/CartContext'
 
-const CartItem = () => {
+const CartItem = ({id, name, qty, price}) => {
+
+    const { dispatch } = useCart();
+
+    const handleIncrement = () => {
+        dispatch({ type: "INCREASE_QUANTITY", payload: {id} })
+    }
+
+    const handleDecrement = () => {
+        dispatch({ type: "DECREASE_QUANTITY", payload: {id} })
+    }
+
+    const handleRemove = () => {
+        dispatch({ type: "REMOVE_MEAL", payload: {id} })
+    }
+    
   return (
-    <li>
-      
+    <li className='cart-item'>
+      <p>
+        {name} - {qty} x {formatCurrency.format(price)}
+      </p>
+
+      <p className='cart-item-actions'>
+        <button onClick={handleDecrement}>-</button>
+        <span>{qty}</span>
+        <button onClick={handleIncrement}>+</button>
+        <button onClick={handleRemove}><Trash2 /></button>
+      </p>
     </li>
   )
 }
