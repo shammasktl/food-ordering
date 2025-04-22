43f542aefe101cfe3fe85add4e627b3dd248aa31
shammasktl <mshammast370737@gmail.com>
2025-04-22 19:07:26 +1200

    feat(Cart): start consuming cart data from CartContext

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Modal from './Modal'
+import { useCart } from '../context/CartContext'
 
 const Cart = () => {
+    const { cart } = useCart()
+
   return (
-    <Modal>
-        
+    <Modal className="cart">
+        <h2>Your Cart</h2>
+
+        <ul>
+            
+        </ul>
     </Modal>
   )
 }
