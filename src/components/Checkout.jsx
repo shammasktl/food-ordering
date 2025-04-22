507edd804c8a840c5740ba59be951c4614e0d4bc
shammasktl <mshammast370737@gmail.com>
2025-04-23 04:18:04 +1200

    refactor(Checkout): use reusable Input component for form fields

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Modal from './Modal'
 import { useCart } from '../context/CartContext'
 import { formatCurrency } from '../util/currencyFormatter'
+import Input from './UI/Input'
 
 const Checkout = () => {
   const { cart } = useCart()
@@ -17,6 +18,14 @@ const Checkout = () => {
       <form>
         <h2>Wrap up your order</h2>
         <p>Total Amount: {formatCurrency.format(total)}</p>
+
+        <Input id="full-name" label="Full Name" type="text" />
+        <Input id="email" label="E-mail Address" type="email" />
+        <Input id="street" label="Street" type="text" />
+        <div className="control-row">
+          <Input id="postal-code" label="Postal Code" type="text"  />
+          <Input id="city" label="City" type="text"  />
+        </div>
       </form>
     </Modal>
   )
