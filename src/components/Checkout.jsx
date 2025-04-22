514738e8ffbbcefe6224a82f9586fc96b837c785
shammasktl <mshammast370737@gmail.com>
2025-04-23 04:47:17 +1200

    feat(Checkout): start implementing form submission logic for checkout process

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Modal from './Modal'
-import { useCart } from '../context/CartContext'
-import { formatCurrency } from '../util/currencyFormatter'
-import Input from './UI/Input'
-import Button from './UI/Button'
-import { useProgress } from '../context/UserProgressContext'
+import React from "react";
+import Modal from "./Modal";
+import { useCart } from "../context/CartContext";
+import { formatCurrency } from "../util/currencyFormatter";
+import Input from "./UI/Input";
+import Button from "./UI/Button";
+import { useProgress } from "../context/UserProgressContext";
 
 const Checkout = () => {
-  const { cart } = useCart()
-  const { progress, progressDispatch } = useProgress()
+  const { cart } = useCart();
+  const { progress, progressDispatch } = useProgress();
 
   const total = cart.reduce(
     (totalPrice, cartedMeal) =>
@@ -18,11 +18,16 @@ const Checkout = () => {
 
   const handleCloseCheckout = () => {
     progressDispatch({ type: "HIDE" });
-  }
+  };
+
+  const handleConfirmOrder = (event) => {
+    event.preventDefault();
+
+  };
 
   return (
-    <Modal open={progress ===  "checkout"} onClose={handleCloseCheckout}>
-      <form>
+    <Modal open={progress === "checkout"} onClose={handleCloseCheckout}>
+      <form onSubmit={handleConfirmOrder} className="checkout-form">
         <h2>Wrap up your order</h2>
         <p>Total Amount: {formatCurrency.format(total)}</p>
 
@@ -30,17 +35,19 @@ const Checkout = () => {
         <Input id="email" label="E-mail Address" type="email" />
         <Input id="street" label="Street" type="text" />
         <div className="control-row">
-          <Input id="postal-code" label="Postal Code" type="text"  />
-          <Input id="city" label="City" type="text"  />
+          <Input id="postal-code" label="Postal Code" type="text" />
+          <Input id="city" label="City" type="text" />
         </div>
 
         <p className="modal-actions">
-          <Button textOnly type="button" onClick={handleCloseCheckout}>Close</Button>
+          <Button textOnly type="button" onClick={handleCloseCheckout}>
+            Close
+          </Button>
           <Button>Confirm Order</Button>
         </p>
       </form>
     </Modal>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
