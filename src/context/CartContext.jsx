744e373c3d022e2ad077c43eff2e76473bcca73e
shammasktl <mshammast370737@gmail.com>
2025-04-22 16:36:46 +1200

    feat(Context): implement ADD_MEAL logic to add new meal or increase quantity if it exists

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,42 +1,46 @@
 import { createContext, useReducer } from "react";
 
-const initialCart = []
+const initialCart = [];
 
 const CartContext = createContext({
   cart: initialCart,
-  dispatch : () => {}
-})
+  dispatch: () => {},
+});
 
 const cartReducer = (state, action) => {
-  const existingMeal = state.find(meal => meal.id === action.meal.id)
-  switch(action.type) {
+  const existingMeal = state.find((meal) => meal.id === action.meal.id);
+  switch (action.type) {
     case "ADD_MEAL":
+      if (existingMeal) {
+        return state.map((meal) =>
+          meal.id === action.payload.id
+            ? { ...meal, quantity: meal.quantity + 1 }
+            : meal
+        );
+      }
 
+      return [...state, {...action.payload, quantity: 1}]
     case "REMOVE_MEAL":
-      // logic to remove meal from cart
+    // logic to remove meal from cart
 
     case "INCREASE_QUANTITY":
-      // logic to increase quantity of meal in cart
+    // logic to increase quantity of meal in cart
 
     case "DECREASE_QUANTITY":
-      // logic to decrease quantity of meal in cart
+    // logic to decrease quantity of meal in cart
 
     case "CLEAR_CART":
-      // logic to clear cart
+    // logic to clear cart
 
     default:
-      return state
+      return state;
   }
-}
+};
 
-const CartProvider = ({children}) => {
-  const [ cart, dispatch] = useReducer(cartReducer, initialCart)
+const CartProvider = ({ children }) => {
+  const [cart, dispatch] = useReducer(cartReducer, initialCart);
 
-  return (
-    <CartContext.Provider>
-      {children}
-    </CartContext.Provider>
-  )
-}
+  return <CartContext.Provider>{children}</CartContext.Provider>;
+};
 
 export default CartProvider;
