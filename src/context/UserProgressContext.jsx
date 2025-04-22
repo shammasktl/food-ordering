88e830f132fd17227589185b57a5f5504519c166
shammasktl <mshammast370737@gmail.com>
2025-04-22 23:23:44 +1200

    feat(UserProgressContext): add reducer function and useUserProgress hook for using context

---
+++ src/context/UserProgressContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useReducer, useContext } from "react";
+
+const initialProgress = ""
+
+
+const UserProgressContext = createContext({
+    progress: initialProgress,
+    progressDispatch: () => {}
+})
+
+const progressReducer = (state, action) => {
+    switch(action.type) {
+        case "SHOW_CART":
+            return "cart"
+
+        case "SHOW_CHECKOUT":
+            return "checkout"
+
+        case "HIDE":
+            return initialProgress
+
+        default:
+            return state
+    }
+}
+
+const UserProgressProvider = ({children}) => {
+
+    const [progress, progressDispatch] = useReducer(progressReducer, initialProgress)
+
+    const progressContextValue = {
+        progress,
+        progressDispatch
+    }
+    return (
+        <UserProgressContext.Provider value={progressContextValue}>
+            {children}
+        </UserProgressContext.Provider>
+    )
+}
+
+export const useUserProgress = () => useContext(UserProgressContext)
+
+export default UserProgressProvider
