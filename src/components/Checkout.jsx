64ac8014dea3282e779b6ffbddd6f8c85578d08e
shammasktl <mshammast370737@gmail.com>
2025-04-23 04:04:43 +1200

    feat(Checkout): display total amount using cart data and format with currencyFormatter

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Modal from './Modal'
 import { useCart } from '../context/CartContext'
+import { formatCurrency } from '../util/currencyFormatter'
 
 const Checkout = () => {
   const { cart } = useCart()
+
+  const total = cart.reduce(
+    (totalPrice, cartedMeal) =>
+      totalPrice + cartedMeal.price * cartedMeal.quantity,
+    0
+  );
+
   return (
     <Modal>
       <form>
         <h2>Wrap up your order</h2>
-        <p>Total Amount: </p>
+        <p>Total Amount: {formatCurrency.format(total)}</p>
       </form>
     </Modal>
   )
