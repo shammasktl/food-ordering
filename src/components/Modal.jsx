4a0b878feedca81aa85280d2b103e801b0f3ab8d
shammasktl <mshammast370737@gmail.com>
2025-04-22 18:51:46 +1200

    feat(Modal): add logic for opening modal dynamically

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,18 @@
-import React, { useRef, useEffect } from 'react'
-import { createPortal } from "react-dom"
+import React, { useRef, useEffect } from "react";
+import { createPortal } from "react-dom";
 
-const Modal = ({children, open}) => {
-    const dialog = useRef()
+const Modal = ({ children, open }) => {
+  const dialog = useRef();
 
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    }
+  }, [open]);
   return createPortal(
-    <dialog ref={dialog}>
-      {children}
-    </dialog>,
+    <dialog ref={dialog}>{children}</dialog>,
     document.getElementById("modal")
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
